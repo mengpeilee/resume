ead5636bc6893aa8e8a3512c8eec48f7390d8de3
mengpeilee <asdfg3865@gmail.com>
2021-12-14 21:50:57 +0800

    style: adjust style

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,6 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2022 Meng Pei Lee</li>
-              <li>CSS Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
         </div>
