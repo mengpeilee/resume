68ea193994c5f8197d0dc9c3033b63e5f2b31728
mengpeilee <asdfg3865@gmail.com>
2023-09-09 22:37:28 -0700

    update resume

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,55 +1,83 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
-      var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
-      })
+      var occupation = this.props.data.occupation;
+      var description = this.props.data.description;
+      var city = this.props.data.address.city;
+      var networks = this.props.data.social.map(function(network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url} target="_blank">
+              <i className={network.className}></i>
+            </a>
+          </li>
+        );
+      });
     }
 
     return (
       <header id="home">
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
 
-      <nav id="nav-wrap">
-
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#motto">motto</a></li>
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Works
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#motto">
+                motto
+              </a>
+            </li>
             {/* <li><a className="smoothscroll" href="#contact">Contact</a></li> */}
-         </ul>
+          </ul>
+        </nav>
 
-      </nav>
-
-      <div className="row banner">
-         <div className="banner-text">
+        <div className="row banner">
+          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3 style={{color: '#fff'}}>
-            A Software Engineer with over 4 years of experience eager to solve real-world problems via the power of software technology.
+            <h3 style={{ color: "#fff" }}>
+              A Software Engineer with over{" "}
+              {new Date().getFullYear() - (new Date().getFullYear() - 5)} years
+              of experience eager to solve real-world problems via the power of
+              software technology.
             </h3>
             <hr />
-            <ul className="social">
-               {networks}
-            </ul>
-         </div>
-      </div>
+            <ul className="social">{networks}</ul>
+          </div>
+        </div>
 
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
-
-   </header>
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
     );
   }
 }
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,92 +1,252 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Resume extends Component {
-  render() {
+  renderWork() {
+    return (
+      <div className="row work">
+        <div className="three columns header-col">
+          <h1>
+            <span>Work</span>
+          </h1>
+        </div>
 
-    if(this.props.data){
+        <div className="nine columns main-col">
+          <div key={"tesla"}>
+            <h3>Tesla</h3>
+            <p className="info">
+              Software Engineer Intern<span>&bull;</span>{" "}
+              <em className="date">May. 2023 – Aug. 2023</em>
+            </p>
+            <p>
+              <span>&bull; </span> Developed a platform from scratch, enabling
+              Data Scientists to build visual web apps without any front-end
+              knowledge, resulting in reduced their web development timelines
+              from one week to just one day.
+            </p>
+            <p>
+              <span>&bull; </span> Migrated and restructured Airflow to support
+              NVIDIA GPU, enhancing scalability for ML model training.
+            </p>
+            <p>
+              <span>&bull; </span> Assisted team in integrating CI/CD pipeline,
+              accelerating development cycles and ensuring seamless updates.
+            </p>
+          </div>
+          <div key={"skywatch"}>
+            <h3>Inventec</h3>
+            <p className="info">
+              Sr. Software Engineer<span>&bull;</span>{" "}
+              <em className="date">Dec. 2019 – May. 2022</em>
+            </p>
+            <p>
+              Inventec is a tier-one manufacturer of laptops and consumer
+              electronic products with annual revenue exceeding US$18B in 2020.
+            </p>
+            <p>
+              <span>&bull; </span>Led{" "}
+              <a
+                href="https://apps.apple.com/tw/app/skywatch-view/id523592053"
+                target="_blank"
+              >
+                APP
+              </a>{" "}
+              team of 2 engineers to integrate react-native with the existing
+              native app, and also using multi-bundler to speed up APP by
+              splitting each bundle file from <strong>4MB to 200 KB</strong>.
+            </p>
+            <p>
+              <span>&bull; </span> Implemented a IoT management system with
+              Kafka to collect data for near real-time suggestion functionality.
+            </p>
+            <p>
+              <span>&bull; </span>Developed{" "}
+              <a href="https://line.me/R/ti/p/%40825xitig" target="_blank">
+                Chatbot
+              </a>{" "}
+              service based on Node.js, Docker and Kubernetes into IoT platform
+              to support customers control IoT devices easily and efficiently.
+            </p>
+            <p>
+              <span>&bull; </span>Created JavaScript{" "}
+              <a
+                href="https://github.com/Skywatch24/JS-Library"
+                target="_blank"
+              >
+                SDK
+              </a>{" "}
+              from scratch to support vendors to develop their IoT Services.{" "}
+              <strong>(number of devices: 50,000+)</strong>
+            </p>
+          </div>
+          <div key={"tico"}>
+            <h3>Tico</h3>
+            <p className="info">
+              Software Engineer<span>&bull;</span>{" "}
+              <em className="date">Feb. 2017 – Nov. 2019</em>
+            </p>
+            <p>
+              Tico focuses on creating human-centered communication solutions to
+              improve collaborations for the world.
+            </p>
+            <p>
+              <span>&bull; </span>Developed communication{" "}
+              <a href="https://tico.app/" target="_blank">
+                service
+              </a>{" "}
+              from scratch based on React Native, with <strong>10,000+</strong>{" "}
+              users. (
+              <a href="https://tico.chat/en" target="_blank">
+                Web
+              </a>
+              ,{" "}
+              <a
+                href="https://itunes.apple.com/app/id1136577997"
+                target="_blank"
+              >
+                iOS
+              </a>
+              ,{" "}
+              <a
+                href="https://play.google.com/store/apps/details?id=com.ticoservice"
+                target="_blank"
+              >
+                Android
+              </a>
+              )
+            </p>
+            <p>
+              <span>&bull; </span>Built testing environment to improve front-end
+              developing quality using Jest and React Testing Library.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderEducation() {
+    return (
+      <div className="row education">
+        <div className="three columns header-col">
+          <h1>
+            <span>Education</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">
+          <div className="row item">
+            <div className="twelve columns">
+              <div key={"MS"}>
+                <h3>Carnegie Mellon University</h3>
+                <p className="info">
+                  Master of Science in Software Engineering<span>&bull;</span>
+                  <em className="date">Aug. 2022 – Dec. 2023</em>
+                  <br />
+                  <em className="date">Cumulative GPA: 3.62 / 4.0</em>
+                </p>
+                <p>
+                  <strong>Coursework</strong>
+                  <br />
+                  <p>
+                    Cloud Computing,
+                    <br />
+                    Foundation of Software Engineering, <br />
+                    Software Design and Architecture, <br />
+                    Software Refactoring <br />
+                    Data Science of Software Engineering
+                  </p>
+                </p>
+              </div>
+            </div>
+            <div className="twelve columns">
+              <div key={"MS"}>
+                <h3>Fu Jen Catholic University</h3>
+                <p className="info">
+                  Master of Science in Computer Science<span>&bull;</span>
+                  <em className="date">Sep. 2017 – Jun. 2018</em>
+                  <br />
+                  <em className="date">Cumulative GPA: 3.97 / 4.0</em>
+                </p>
+                <p>
+                  <strong>PUBLICATION: </strong>
+                  <br />
+                  <strong>Meng-Pei Lee</strong>, Hsiang-Yu Hu, Hsing Mei, “
+                  <a
+                    href="https://dl.acm.org/doi/abs/10.1145/3242789.3242834"
+                    target="_blank"
+                  >
+                    Life Chasing: A Location-based Game Prototype for Elderly
+                    Health Promotion
+                  </a>
+                  ”, International Conference on Healthcare Service Management (
+                  <strong>ACM</strong>), Japan, 2018, pp. 265-271
+                </p>
+              </div>
+            </div>
+            <div className="twelve columns">
+              <div key={"BA"}>
+                <h3>Fu Jen Catholic University</h3>
+                <p className="info">
+                  Bachelor of Science in Computer Science<span>&bull;</span>
+                  <em className="date">Sep. 2013 – Jun. 2017</em>
+                  <br />
+                  <em className="date">GPA in final year: 4.0 / 4.0</em>
+                </p>
+                <p>
+                  <strong>HONOR: </strong>
+                  <br />
+                  Won first place in{" "}
+                  <a
+                    href="https://github.com/mengpeilee/Fit-Meets"
+                    target="_blank"
+                  >
+                    Graduation Competition
+                  </a>{" "}
+                  for developing a dating app combined with an IoT device.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    if (this.props.data) {
       var skillmessage = this.props.data.skillmessage;
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
-      })
+      var skills = this.props.data.skills.map(function(skills) {
+        var className = "bar-expand " + skills.name.toLowerCase();
+        return (
+          <li key={skills.name}>
+            <span style={{ width: skills.level }} className={className}></span>
+            <em>{skills.name}</em>
+          </li>
+        );
+      });
     }
 
     return (
       <section id="resume">
+        {this.renderWork()}
+        {this.renderEducation()}
 
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                <div key={"MS"}><h3>Carnegie Mellon University</h3>
-                  <p className="info">Master of Science in Software Engineering<span>&bull;</span><em className="date">Aug. 2022 – Present</em></p>
-                  <p><strong>Looking for a 2023 summer internship.</strong></p>
-                </div>
-               </div>
-               <div className="twelve columns">
-                <div key={"MS"}><h3>Fu Jen Catholic University</h3>
-                  <p className="info">Master of Science in Computer Science<span>&bull;</span><em className="date">Sep. 2017 – Jun. 2018</em><br /><em className="date">Cumulative GPA: 3.97 / 4.0</em></p>
-                  <p><strong>PUBLICATION: </strong><br /><strong>Meng-Pei Lee</strong>, Hsiang-Yu Hu, Hsing Mei, “<a href="https://dl.acm.org/doi/abs/10.1145/3242789.3242834" target="_blank">Life Chasing: A Location-based Game Prototype for Elderly Health Promotion</a>”, International Conference on Healthcare Service Management (<strong>ACM</strong>), Japan, 2018, pp. 265-271</p>
-                </div>
-               </div>
-               <div className="twelve columns">
-                <div key={"BA"}><h3>Fu Jen Catholic University</h3>
-                  <p className="info">Bachelor of Science in Computer Science<span>&bull;</span><em className="date">Sep. 2013 – Jun. 2017</em><br /><em className="date">GPA in final year: 4.0 / 4.0</em></p>
-                  <p><strong>HONOR: </strong><br />Won first place in <a href="https://github.com/mengpeilee/Fit-Meets" target="_blank">Graduation Competition</a> for developing a dating app combined with an IoT device.</p>
-                </div>
-               </div>
-            </div>
-         </div>
-      </div>
-
-
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          <div key={"skywatch"}><h3>Inventec & Skywatch Innovation Inc.</h3>
-              <p className="info">Sr. Software Engineer<span>&bull;</span> <em className="date">Dec. 2019 – Present</em></p>
-              <p><span>&bull; </span>Led <a href="https://www.skywatch24.com/home-en" target="_blank">front-end</a> team of 2 engineers to separate front-end (JS, jQuery, React) from complicated back-end (PHP, Python, Node.js) architecture using Webpack and Lerna (Monorepo), which led to <strong>a 50% increase </strong> in development speed.</p>
-              <p><span>&bull; </span>Led <a href="https://apps.apple.com/tw/app/skywatch-view/id523592053" target="_blank">APP</a> team of 2 engineers to integrate react-native with the existing native app, and also using multi-bundler to speed up APP by splitting each bundle file from <strong>4MB to 200 KB</strong>.</p>
-              <p><span>&bull; </span>Built <a href="https://line.me/R/ti/p/%40825xitig" target="_blank">Chatbot</a> server that can reply <strong>thousands of users at the same time</strong> by using Docker and Kubernetes on GCP.</p>
-              <p><span>&bull; </span>Project owner for <a href="https://github.com/Skywatch24/JS-Library" target="_blank">SDK</a> that supports enterprises in developing their IoT Services. <strong>(number of devices: 50,000+)</strong></p>
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
           </div>
-          <div key={"tico"}><h3>Tico</h3>
-              <p className="info">Software Engineer<span>&bull;</span> <em className="date">Feb. 2017 – Nov. 2019</em></p>
-              <p><span>&bull; </span>Project owner for a cross-platform social communication <a href="https://tico.app/" target="_blank">service</a> using React Native, with an average of <strong>10,000+</strong> users per month. (<a href="https://tico.chat/en" target="_blank">Web</a>, <a href="https://itunes.apple.com/app/id1136577997" target="_blank">iOS</a>, <a href="https://play.google.com/store/apps/details?id=com.ticoservice" target="_blank">Android</a>)</p>
-              <p><span>&bull; </span>Built testing environment to improve front-end developing quality using Jest and React Testing Library.</p>
+
+          <div className="nine columns main-col">
+            <p>{skillmessage}</p>
+
+            <div className="bars">
+              <ul className="skills">{skills}</ul>
+            </div>
           </div>
         </div>
-    </div>
-
-
-
-      <div className="row skill">
-
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
-   </section>
+      </section>
     );
   }
 }
